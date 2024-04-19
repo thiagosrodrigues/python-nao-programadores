14f290219824874798ab201183e3d93a51f98476
thiagosrodrigues <62563805+thiagosrodrigues@users.noreply.github.com>
2024-04-19 14:10:56 +0000

    Exercicios

--- exercicios/1-primeiros-passos/4-operadores.py
+++ exercicios/1-primeiros-passos/4-operadores.py
@@ -2,7 +2,7 @@ ano_nascimento = 1989
 ano_formatura = 2010
 
 # Considerando que as variáveis acima correspondem a 'Gerlaine', descubra a idade dela no ano da sua formatura
-
+ano_formatura - ano_nascimento
 
 # Escreva expressões comparativas usando os operadores relacionais >, <= e ==. Imprima na tela as respostas
 

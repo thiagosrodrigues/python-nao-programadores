9a61a3fa965bbd55aac73c02e632d51fcfa738c4
thiagosrodrigues <62563805+thiagosrodrigues@users.noreply.github.com>
2024-05-07 02:03:22 +0000

    Resolucao Exercicios

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -5,6 +5,7 @@ mista = [1, 'python', numerica]
 # Imprima na tela apenas os 5 primeiros elementos da lista
 print(numerica[0:5:1])
 # Crie um slice na lista para que imprima na tela os elementos de índice par
+# slice[inicio:fim:passo] por exemplo numerica[0:-1:2] O python entende o -1 como o último iten da lista
 print(numerica[0:5:2])
 # Remova da lista o último item
 numerica.pop()

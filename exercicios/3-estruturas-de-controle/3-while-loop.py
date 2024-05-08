94f622e99119b57b1d1a14b54b54c5a37a2090df
thiagosrodrigues <62563805+thiagosrodrigues@users.noreply.github.com>
2024-05-08 19:14:35 +0000

    #

--- exercicios/3-estruturas-de-controle/3-while-loop.py
+++ exercicios/3-estruturas-de-controle/3-while-loop.py
@@ -1,7 +1,13 @@
 # Crie duas variáveis do tipo numérica, uma sinalizando a fase atual do curso e outra o nível final
 
+list(range(10))
 
 # Crie um while loop que imprima na tela o nível atual
 
 
 # Insira "else" no while loop anterior.
+
+for item in range(4,7):
+  print(f'{item} - Estou aprendendo python')
+else:
+  print('Fim do Loop')
--- exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
+++ exercicios/3-estruturas-de-controle/projeto-fizz-buzz.py
@@ -6,3 +6,18 @@
 # 3.2 Caso o número seja divisível por 5, substitua-o por "Buzz"
 # 3.3 Caso o número seja divisível por 3 e 5, substitua-o por "FizzBuzz"
 
+lista = list(range(15,31))
+indice = 0
+
+for numero in lista:
+  if numero % 3 == 0 and numero % 5 == 0:
+    lista[indice] = 'FizzBuzz'
+  elif numero % 3 == 0:
+    lista[indice] = 'Fizz'
+  elif numero % 5 == 0:
+    lista[indice] = 'Buzz'
+  else:
+    lista[indice] = numero
+  indice += 1
+
+print(lista)

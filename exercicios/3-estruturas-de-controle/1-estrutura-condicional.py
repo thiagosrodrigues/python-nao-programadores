493ae713336a490dba46dc6eebeb31df9cfbe206
thiagosrodrigues <62563805+thiagosrodrigues@users.noreply.github.com>
2024-05-08 02:42:24 +0000

    #

--- exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
+++ exercicios/3-estruturas-de-controle/1-estrutura-condicional.py
@@ -1,8 +1,14 @@
 # Declare 4 variáveis do tipo numérica
-
+x = 6
+y = 12
+z = 9
+w = 78
 
 # Crie uma estrutura condicional para comparar dois números
-
+if (x > y):
+  print(f'{x} é maior que {y}')
+else:
+  print(f'{x} é menor que {y}')
 
 # Se a condição for verdadeira, imprima na tela uma mensagem informando que a condição foi cumprida e informando o número de maior valor
 
--- exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
+++ exercicios/3-estruturas-de-controle/projeto-avaliacao-cursos.py
@@ -5,3 +5,24 @@
 # 4. Crie uma estrutura condicional para verificar se cada variável está contida na lista
 # 5. Se o curso estiver na lista, solicite uma nota para avaliação
 # 6. Armazene essa nota no dicionário, sendo a chave o título do curso e o valor a nota
+
+cursos = ['python', 'sql', 'excel', 'python: basico', 'programação']
+curso_python = 'python'
+curso_git = 'git'
+curso_excel = 'excel'
+
+notas_cursos = {}
+
+if curso_python in cursos:
+  print(f"O curso {curso_python} consta no catálogo. Por favor avalie o curso")
+  notas_cursos[curso_python] = int(input('Qual a nota do curso? '))
+if curso_git in cursos:
+  print(f"O curso {curso_git} consta no catálogo. Por favor avalie o curso")
+  notas_cursos[curso_git] = int(input('Qual a nota do curso? '))
+if curso_excel in cursos:
+  print(f"O curso {curso_excel} consta no catálogo. Por favor avalie o curso")
+  notas_cursos[curso_python] = int(input('Qual a nota do curso? '))
+else:
+  print("Infelizmente curso não consta catálogo.")
+
+  print(notas_cursos)
